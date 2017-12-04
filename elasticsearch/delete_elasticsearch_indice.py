4169109f881130abe731014404f4a0806aab52eb
DinoLai <dinos80152@gmail.com>
2017-12-05 01:03:38 +0800

    :pencil2: fix typo

--- elasticsearch/delete_elasticsearch_indice.py
+++ elasticsearch/delete_elasticsearch_indice.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     elastic_search_endpoint = os.environ['ELASTICSEARCH_ENDPOINT']
     deleteDate = date.today() - timedelta(days=31)
     indice = 'cwl-' + deleteDate.strftime('%Y.%m.%d')
-    url = elastic_search_endpoint + indice
+    url = elastic_search_endpoint + "/" +indice
     cmd = 'curl -XDELETE ' + url
     r = os.popen(cmd).read()
     return r
